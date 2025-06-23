d287d225c13c9df239625ab8e5804bccc064e34e
Daniel Jonguitud <danieljonguitud17@outlook.com>
2025-06-22 20:03:10 -0600

    fix entrypoint shebang bash

--- serverless/doc-reader/src/entrypoint.sh
+++ serverless/doc-reader/src/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 set -e # Exit immediately if a command exits with a non-zero status.
 set -o pipefail # The return value of a pipeline is the status of the last command to exit with a non-zero status
 
